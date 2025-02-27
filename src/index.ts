a93b74fbab6700c1be21479f048abeda36368c0c
You Young June <youngjune@planorban.com>
2025-02-27 14:31:17 -0500

    api ready with authentication

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  console.log("Server running.");
+});
+
+const MONGO_URL = process.env.CONNECTION_STRING;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
